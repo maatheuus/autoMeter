7755ca79842518456875026080e0c9a1c97fe888
MATHEUS <matts14smkd@gmail.com>
2024-08-30 12:24:55 -0300

    add limit for the image

--- backend/app.ts
+++ backend/app.ts
@@ -4,11 +4,19 @@ dotenv.config({ path: "./config.env" });
 import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
+import { router as routerGemini } from "./routes/geminiRoutes";
 
 const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+// Routes
+app.use("/api/v1/", routerGemini);
 
 export default app;
