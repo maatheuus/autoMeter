a057729601a1b31b374a347cbdf7da225e49e112
MATHEUS <matts14smkd@gmail.com>
2024-08-30 22:17:48 -0300

    refac the component

--- src/components/MeasurementForm.tsx
+++ src/components/MeasurementForm.tsx
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
+import { MeasurementFormProps } from "../interfaces";
 
-interface MeasurementFormProps {
-  onSubmit: (data: {
-    customer_code: string;
-    measure_type: "WATER" | "GAS";
-    measure_datetime: string;
-  }) => void;
-}
-
-const MeasurementForm: React.FC<MeasurementFormProps> = ({ onSubmit }) => {
+const MeasurementForm: React.FC<MeasurementFormProps> = ({
+  onSubmit,
+  handleFinalSubmit,
+}) => {
   const [customerCode, setCustomerCode] = useState<string>("");
   const [measureType, setMeasureType] = useState<"WATER" | "GAS">("WATER");
   const [measureDatetime, setMeasureDatetime] = useState<string>("");
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     onSubmit({
       customer_code: customerCode,
       measure_type: measureType,
@@ -23,44 +19,58 @@ const MeasurementForm: React.FC<MeasurementFormProps> = ({ onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="measurement-form">
-      <div className="form-group">
-        <label htmlFor="customer_code">Customer Code:</label>
-        <input
-          type="text"
-          id="customer_code"
-          value={customerCode}
-          onChange={(e) => setCustomerCode(e.target.value)}
-          required
-        />
-      </div>
+    <div className="bg-white p-8 rounded shadow-md max-w-sm w-full mx-auto md:max-h-96">
+      <h1 className="text-2xl font-bold mb-4">Envio de Dados</h1>
+      <form onSubmit={handleSubmit} className="measurement-form">
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">
+            Código do Usuário
+          </label>
+          <input
+            type="text"
+            value={customerCode}
+            onChange={(e) => setCustomerCode(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
+            placeholder="Digite o código do usuário"
+          />
+        </div>
 
-      <div className="form-group">
-        <label htmlFor="measure_type">Measure Type:</label>
-        <select
-          id="measure_type"
-          value={measureType}
-          onChange={(e) => setMeasureType(e.target.value as "WATER" | "GAS")}
-          required
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">
+            Tipo de Imagem
+          </label>
+          <select
+            value={measureType}
+            onChange={(e) => setMeasureType(e.target.value as "WATER" | "GAS")}
+            required
+            className="w-full p-2 border border-gray-300 rounded"
+          >
+            <option value="all">All</option>
+            <option value="WATER">Water</option>
+            <option value="GAS">Gas</option>
+          </select>
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">Data</label>
+          <input
+            type="datetime"
+            value={measureDatetime}
+            onChange={(e) => setMeasureDatetime(e.target.value)}
+            required
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+        </div>
+
+        <button
+          type="submit"
+          onClick={handleFinalSubmit}
+          className="w-full p-2  text-white font-bold rounded bg-blue-500 transition-all duration-300 hover:bg-blue-700"
         >
-          <option value="WATER">Water</option>
-          <option value="GAS">Gas</option>
-        </select>
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="measure_datetime">Measure Date and Time:</label>
-        <input
-          type="datetime-local"
-          id="measure_datetime"
-          value={measureDatetime}
-          onChange={(e) => setMeasureDatetime(e.target.value)}
-          required
-        />
-      </div>
-
-      <button type="submit">Submit</button>
-    </form>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
