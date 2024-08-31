bba201e38dc74469a45c32f5e10e3615dbcfcbe2
MATHEUS <matts14smkd@gmail.com>
2024-08-30 22:17:28 -0300

    creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import FormData from "./components/FormData";
+import HistoryMeasures from "./components/HistoryMeasures";
 
 function App() {
   return (
-    <>
-      <FormData />
-    </>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <nav className="bg-blue-600 p-4 text-white">
+          <Link to="/" className="mr-4 hover:underline">
+            Formulário de Medição
+          </Link>
+          <Link to="/list" className="hover:underline">
+            Histórico de Medições
+          </Link>
+        </nav>
+        <div className="p-4">
+          <Routes>
+            <Route path="/" element={<FormData />} />
+            <Route path="/list" element={<HistoryMeasures />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
