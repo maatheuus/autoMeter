7d06a8c6c525c8c50e22adb51dde7ae0b7c2b94c
MATHEUS <matts14smkd@gmail.com>
2024-08-30 22:17:15 -0300

    create apis for handle with the datas in front-end

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,10 +6,12 @@ export const uploadImage = async (data: object) => {
   try {
     const res = await axios.post(`${url}/upload`, data);
 
-    console.log("RES", res);
     return res.data;
   } catch (error) {
     console.log(error);
+    throw new Error(
+      `Something wen wrong while upload the data: ${error.message}`
+    );
   }
 };
 
@@ -18,8 +20,35 @@ export const confirmedData = async (data: object) => {
     const res = await axios.patch(`${url}/confirm`, data);
 
     console.log("RES", res);
+    return res;
+  } catch (error) {
+    console.log(error);
+    throw new Error(
+      `Something went wrong while confirm the data: ${error.message}`
+    );
+  }
+};
+
+export const getMeasuresList = async (
+  customer_code: string,
+  measure_type: string | undefined
+) => {
+  try {
+    let query;
+
+    if (measure_type !== undefined) {
+      query = `${url}/${customer_code}/list?measure_type=${measure_type}`;
+    } else {
+      query = `${url}/${customer_code}/list`;
+    }
+    const res = await axios.get(query);
+
+    console.log(res);
     return res.data;
   } catch (error) {
     console.log(error);
+    throw new Error(
+      `Something went wrong while get the data: ${error.message}`
+    );
   }
 };
